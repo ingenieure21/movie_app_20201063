966690d5f9bab802d9d287ff7795b37ec4b43072
Kiyeon Nam <kynam0318@gmail.com>
2022-06-04 03:18:45 +0900

    07장까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,62 +1,50 @@
-import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://a.cdn-hotels.com/gdcs/production188/d1042/560f847f-c64a-4009-9422-1723c2739c36.jpg?impolicy=fcrop&w=1600&h=1066&q=medium',
-    rating: 5
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://a.cdn-hotels.com/gdcs/production125/d1185/1b45666a-9a1b-4770-aff7-cd230004670a.jpg?impolicy=fcrop&w=1600&h=1066&q=medium',
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://a.cdn-hotels.com/gdcs/production12/d29/cc49915a-a856-4e94-9ba4-c7e89a652d2e.jpg?impolicy=fcrop&w=1600&h=1066&q=medium',
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nextinsight.net%2Fimages%2Fstories%2F2015_MYCC%2FMa_Maison%2FTonkatsu%2Fmamaisontonkatsu_millefeuille.jpg&f=1&nofb=1',
-    rating: 4.5
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.foodrepublic.com%2Fwp-content%2Fuploads%2F2014%2F10%2FKorean.food-Kimbap-03.jpg&f=1&nofb=1',
-    rating: 5
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
   }
-];
-
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish =>
-      <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-    )}
-  </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+        )}
+      </section>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 
 export default App;
